2f6a851f447831a06012c44fa19ac3e8f04ad1bf
Leo <eta.development.leo@outlook.com>
2023-08-24 08:22:42 +0000

    dev container

---
+++ .devcontainer/post-install.sh
@@ -0,0 +1,7 @@
+git init
+git config --global --add safe.directory /workspaces/eta-regulator-board
+git remote add -t \* -f origin https://github.com/leontev-vyacheslav/eta-regulator-board
+git checkout master
+git reset --hard
+
+sudo apt-get install iputils-ping -y
