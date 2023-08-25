b72efd6d38e7c49ec1bd09b7fe82e541f8e5a4e5
Вячеслав Леонтьев <70220844+leontev-vyacheslav@users.noreply.github.com>
2023-08-25 10:12:08 +0300

    Update post-install.sh

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,14 +1,23 @@
+### init git and get projects from github
 git init
 git config --global --add safe.directory /workspaces/eta-regulator-board
 git remote add -t \* -f origin https://github.com/leontev-vyacheslav/eta-regulator-board
 git checkout master
 git reset --hard
 
+### install ping utils
 sudo apt-get update
 sudo apt-get install iputils-ping -y
 
 ssh-keygen # install keys for vscode user
 
-sudo apt-get install npm
-
+### Ppermissions for powershell
 sudo setcap cap_net_raw=eip /opt/microsoft/powershell/7/pwsh   # https://github.com/PowerShell/PowerShell/issues/18585
+
+### init web-api
+# install vscode python extention v2022.8.0 for python 3.6 debugging
+# use python instedd python3 sym link that may be reffered to python 3.9 by default
+cd web-api
+python3 -m venv .venv
+source .venv/bin/activate
+python -m pip install -r requirements.txt
