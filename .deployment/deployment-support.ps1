673f1823935aee6011d3914327a4da0a64b7061b
Leo <leontev.vyacheslav@gmail.com>
2025-02-27 11:02:50 +0000

    feat: deploying improvement

--- .deployment/deployment-support.ps1
+++ .deployment/deployment-support.ps1
@@ -1,16 +1,13 @@
 $GREEN = "`e[32m"
 $RESET = "`e[0m"
-$IPADDR = "omega-7e12"
-#$IPADDR = "omega-8f79"
 $ACCOUNT = 'root'
-$WORKSPACE_ROOT = "/mnt/mmcblk0p1/eta-regulator-board" # /home/eta-regulator-board
+$WORKSPACE_ROOT = "/mnt/mmcblk0p1/eta-regulator-board"
 
 $utcNow = Get-Date -Format "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'" -AsUTC
 $buildDateTimeMark = Get-Date -Format "yyyyMMdd-HHmmss"
 
 $WEB_API_APP_NAME = "eta-regulator-board-web-api"
 $WEB_UI_APP_NAME = "eta-regulator-board-web-ui"
-$WEB_API_SHUTDOWN_ENDPOINT = "http://127.0.0.1:5000/shutdown"
 
 function Set-AppVersion([string] $RelativePath, [string] $SearchPattern, [string] $Substitution)
 {
